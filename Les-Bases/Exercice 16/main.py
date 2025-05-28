fd271517c3904c64f5d9cce8d7dd0107e3cb18d8
GalsenCode <galsencode@gmail.com>
2025-05-28 21:39:26 +0000

    Rectification on devrait utiliser while au lieu de for

--- Les-Bases/Exercice 16/main.py
+++ Les-Bases/Exercice 16/main.py
@@ -1,8 +1,12 @@
 """
 Ecrivez un programme en Python qui calcule la somme de 1 à 10. En utilisant la boucle "while".
 """
-somme  = 0
-for i in range(1,11) :
+
+somme = 0
+i = 1
+
+while i <= 10:
     somme += i
+    i += 1
 
 print(f"La somme de 1 à 10 est {somme}")
