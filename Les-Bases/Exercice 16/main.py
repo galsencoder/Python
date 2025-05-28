c547746baeea8fca81c61b408d3fbeeffa2bdfe8
GalsenCode <galsencode@gmail.com>
2025-05-28 21:36:37 +0000

    La somme de 1 à 10

---
+++ Les-Bases/Exercice 16/main.py
@@ -0,0 +1,8 @@
+"""
+Ecrivez un programme en Python qui calcule la somme de 1 à 10. En utilisant la boucle "while".
+"""
+somme  = 0
+for i in range(1,11) :
+    somme += i
+
+print(f"La somme de 1 à 10 est {somme}")
