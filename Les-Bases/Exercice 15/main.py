6260abc9db8f057850d104a7815e272ae93e1c4d
GalsenCode <galsencode@gmail.com>
2025-05-28 02:42:44 +0000

    La moyenne de trois nombres

---
+++ Les-Bases/Exercice 15/main.py
@@ -0,0 +1,9 @@
+"""
+Écrire un programme Python qui calcule la moyenne de trois nombres saisis par l'utilisateur.
+"""
+
+a = float(input("Entrez le premier nombre : "))
+b = float(input("Entrez le deuxieme nombre : "))
+c = float(input("Entrez le troisieme nombre : "))
+
+print(f"La moyenne est {(a+b+c)/3}")
