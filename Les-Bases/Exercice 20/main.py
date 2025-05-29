b7d5cf395558a91e63f2fdb54d28fcf5c8e4a0fb
GalsenCode <galsencode@gmail.com>
2025-05-29 01:20:07 +0000

    TM

--- Les-Bases/Exercice 20/main.py
+++ Les-Bases/Exercice 20/main.py
@@ -1,5 +1,6 @@
 """
-Écrire un programme en Python qui affiche la table de multiplication d'un entier saisi par l'utilisateur, en utilisant la boucle "for".
+Écrire un programme en Python qui affiche la table de multiplication
+d'un entier saisi par l'utilisateur, en utilisant la boucle "for".
 """
 a = int(input("Entrez un entier positif : "))
 
@@ -7,4 +8,4 @@ print(f"La table de multiplication de {a} : ")
 multi = 1
 
 for i in range(11) :
-    print(f"{a} x {i} = {a*i}")
+    print(f"{a} x {i} = {a*i}")  
