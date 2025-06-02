e6e3c6733d2faa85cb021eb2780acae2c0a1640f
GalsenCode <galsencode@gmail.com>
2025-06-02 23:48:16 +0000

    Affiche n fois Hello

---
+++ Débutant/Exercice 29/main.py
@@ -0,0 +1,11 @@
+"""
+Écrire un programme en Python qui affiche n fois « Hello ».
+En utilisant la boucle « while ».
+"""
+
+
+n = int(input("Entrez un nombre: "))
+i = 0
+while i < n:
+    print("Hello")
+    i += 1
