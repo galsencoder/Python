645cd64d92ea717ffec0cfd943d69a083292a859
GalsenCode <galsencode@gmail.com>
2025-06-02 23:50:47 +0000

    Affiche n fois Galsen Code en utilisant for

---
+++ Débutant/Exercice 30/main.py
@@ -0,0 +1,9 @@
+"""
+Écrire un programme en Python qui affiche n fois "Galsen Code".
+En utilisant la boucle "for".
+"""
+
+n = int(input("Entrez un nombre : "))
+
+for i in range(n):
+    print("Galsen Code")
