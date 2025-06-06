77aff20567b50f037f98e5e9b9f17a9ce1aa75bf
GalsenCode <galsencode@gmail.com>
2025-06-06 23:13:31 +0000

    Decompression d'un tuple en plusieurs variables

---
+++ Tuple/Programme 1/main.py
@@ -0,0 +1,14 @@
+"""
+Écrire un programme Python pour décompresser un tuple en plusieurs variables.
+"""
+
+# Définition du tuple
+mon_tuple = (1, 2, 3)
+
+# Décompression du tuple en plusieurs variables
+a, b, c = mon_tuple
+
+# Affichage des variables décompressées
+print("a =", a)
+print("b =", b)
+print("c =", c)
