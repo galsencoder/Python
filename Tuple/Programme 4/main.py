f8a56fdc627ea4096b5e62f0d5c6e97e05c4b526
GalsenCode <galsencode@gmail.com>
2025-06-06 23:22:12 +0000

    Obtenir un element dans un tuple

---
+++ Tuple/Programme 4/main.py
@@ -0,0 +1,12 @@
+"""
+Ecrivez un programme Python pour obtenir le troisième élément à partir du dernier élément d’un tuple.
+"""
+
+# Définition du tuple
+mon_tuple = ('G', 'A', 'L', 'S', 'E', 'N')
+
+# Obtention du troisième élément à partir du dernier élément
+troisieme_element = mon_tuple[-3]
+
+# Affichage du troisième élément
+print(troisieme_element)  
