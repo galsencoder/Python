080111934165c330d8a407602203280f281f9e79
GalsenCode <galsencode@gmail.com>
2025-06-06 23:19:49 +0000

    Conversion d'un tuple en chaine de caractere

---
+++ Tuple/Programme 3/main.py
@@ -0,0 +1,12 @@
+"""
+Écrire un programme Python pour convertir un tuple en chaîne de caractères.
+"""
+
+# Définition du tuple
+mon_tuple = ('G', 'A', 'L', 'S', 'E', 'N')
+
+# Conversion du tuple en chaîne de caractères
+ma_chaine = ''.join(mon_tuple)
+
+# Affichage de la chaîne de caractères
+print(ma_chaine)
