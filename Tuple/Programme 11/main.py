9a2ab27aa01e632fb241be487b87fad96e3f3d42
GalsenCode <galsencode@gmail.com>
2025-06-07 20:24:26 +0000

    La longueur d'un tuple

---
+++ Tuple/Programme 11/main.py
@@ -0,0 +1,12 @@
+"""
+Ecrivez un programme Python pour trouver la longueur d’un tuple.
+"""
+
+# Définition du tuple
+mon_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# Trouver la longueur du tuple
+longueur = len(mon_tuple)
+
+# Afficher la longueur
+print("La longueur du tuple est :", longueur)
