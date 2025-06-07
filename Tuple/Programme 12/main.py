7c17b410478791bda32180cb2f609520dd39c6b0
GalsenCode <galsencode@gmail.com>
2025-06-07 20:28:15 +0000

    Conversion d'un tuple en dictionnaire

---
+++ Tuple/Programme 12/main.py
@@ -0,0 +1,12 @@
+"""
+Écrire un programme Python pour convertir un tuple en dictionnaire.
+"""
+
+# Définition du tuple
+mon_tuple = (("a", 1), ("b", 2), ("c", 3))
+
+# Conversion du tuple en dictionnaire
+mon_dictionnaire = dict(mon_tuple)
+
+# Affichage du dictionnaire
+print("Le dictionnaire est :", mon_dictionnaire)
