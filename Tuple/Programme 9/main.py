e145285e7761a570ca4e21cea0e7197e225aed0c
GalsenCode <galsencode@gmail.com>
2025-06-07 12:52:52 +0000

    Decouper un tuple

---
+++ Tuple/Programme 9/main.py
@@ -0,0 +1,13 @@
+"""
+Écrire un programme Python pour découper un tuple.
+"""
+
+# Définition du tuple
+mon_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# Découpage du tuple
+sous_tuple = mon_tuple[2:5]
+
+# Affichage du résultat
+print("Le tuple d'origine :", mon_tuple)
+print("Le sous-tuple découpé :", sous_tuple)
