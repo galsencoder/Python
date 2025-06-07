b424252eca81279cdfb84d794d2828eadbef887d
GalsenCode <galsencode@gmail.com>
2025-06-07 20:22:17 +0000

    L'index d'un element dans un tuple

---
+++ Tuple/Programme 10/main.py
@@ -0,0 +1,13 @@
+"""
+Ecrivez un programme Python pour trouver l’index d’un élément dans un tuple.
+"""
+
+# Définition du tuple
+mon_tuple = (10, 20, 30, 40, 50)
+
+# Trouver l'index d'un élément
+element = 30
+index = mon_tuple.index(element)
+
+# Afficher l'index
+print("L'index de l'élément", element, "est :", index)
