f04b44db8e6e6cd6a43010382ca45e9b317c6d7a
GalsenCode <galsencode@gmail.com>
2025-06-07 12:46:42 +0000

    Conversion liste en tuple

---
+++ Tuple/Programme 7/main.py
@@ -0,0 +1,13 @@
+"""
+Écrire un programme Python pour convertir une liste en un tuple.
+"""
+
+# Définition de la liste
+ma_liste = [1, 2, 3, 4, 5]
+
+# Conversion de la liste en tuple
+mon_tuple = tuple(ma_liste)
+
+# Affichage du résultat
+print("La liste d'origine :", ma_liste)
+print("Le tuple converti :", mon_tuple)
