f92124689f2c25ba28f232c7608fbe75979117e4
GalsenCode <galsencode@gmail.com>
2025-06-09 23:36:14 +0000

    Ensemble vide et un ensemble avec trois elements

---
+++ Set (Ensembles)/Programme 1/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour créer un ensemble vide et un ensemble avec 3 éléments.
+"""
+
+# Création d'un ensemble vide
+ensemble_vide = set()
+
+# Création d'un ensemble avec 3 éléments
+ensemble_avec_elements = {1, 2, 3}
+
+# Affichage des ensembles
+print("Ensemble vide :", ensemble_vide)
+print("Ensemble avec éléments :", ensemble_avec_elements)
