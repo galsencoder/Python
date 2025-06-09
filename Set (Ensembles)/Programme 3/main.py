515aedb7ae4b5e12e3ef2f16961e77e304beb742
GalsenCode <galsencode@gmail.com>
2025-06-09 23:42:38 +0000

    Programme ajout et suprression d'un element

---
+++ Set (Ensembles)/Programme 3/main.py
@@ -0,0 +1,15 @@
+﻿"""
+Écrire un programme Python pour ajouter un ou plusieurs membres à un ensemble.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3}
+
+# Ajout d'un élément à l'ensemble
+ensemble.add(4)
+
+# Ajout de plusieurs éléments à l'ensemble
+ensemble.update([5, 6, 7])
+
+# Affichage de l'ensemble modifié
+print("Ensemble après ajout d'éléments :", ensemble)
---
+++ Set (Ensembles)/Programme 4/main.py
@@ -0,0 +1,15 @@
+﻿"""
+Écrire un programme Python pour supprimer un ou plusieurs éléments d’un ensemble donné.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3, 4, 5, 6}
+
+# Suppression d'un élément de l'ensemble
+ensemble.remove(4)
+
+# Suppression de plusieurs éléments de l'ensemble
+ensemble.difference_update({2, 3})
+
+# Affichage de l'ensemble modifié
+print("Ensemble après suppression d'éléments :", ensemble)
