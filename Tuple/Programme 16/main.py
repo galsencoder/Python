71f93166a655579a3687d08cc2a82e8abbe8b398
GalsenCode <galsencode@gmail.com>
2025-06-09 23:29:41 +0000

    Supprime un ou plusieurs tuples

---
+++ Tuple/Programme 16/main.py
@@ -0,0 +1,12 @@
+﻿"""
+Écrire un programme Python pour supprimer un ou plusieurs tuple(s) vide(s) d’une liste de tuple(s).
+"""
+
+# Définition de la liste de tuples
+tuples_list = [(1, 2, 3), (), (4, 5), (), (6, 7, 8)]
+
+# Suppression des tuples vides
+tuples_list = [t for t in tuples_list if t]
+
+# Affichage de la liste modifiée
+print(tuples_list)
