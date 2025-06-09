2e5acfa1d98cd18966aa8958bc6d44d2ccafcb6b
GalsenCode <galsencode@gmail.com>
2025-06-09 23:38:03 +0000

    Parcourir un ensemble

---
+++ Set (Ensembles)/Programme 2/main.py
@@ -0,0 +1,11 @@
+﻿"""
+Écrire un programme Python pour parcourir un ensemble en utilisant la boucle for.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3, 4, 5}
+
+# Parcours de l'ensemble avec une boucle for
+for element in ensemble:
+    print("Élément :", element)
+
