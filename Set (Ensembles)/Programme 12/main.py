51bc59a7f42d25f45044011e182719d5cdf32efc
GalsenCode <galsencode@gmail.com>
2025-06-10 22:54:44 +0000

    Vider un ensemble

---
+++ Set (Ensembles)/Programme 12/main.py
@@ -0,0 +1,12 @@
+﻿"""
+Écrire un programme Python pour supprimer tous les éléments d’un ensemble donné.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3, 4, 5}
+
+# Suppression de tous les éléments de l'ensemble
+ensemble.clear()
+
+# Affichage du résultat
+print("L'ensemble après suppression de tous les éléments est :", ensemble)
