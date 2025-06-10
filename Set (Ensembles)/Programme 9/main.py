1a1c5e314e1a5f8468b5d93ec2f9af632a6a2adb
GalsenCode <galsencode@gmail.com>
2025-06-10 22:47:22 +0000

    Difference Symetrique Ensemble

---
+++ Set (Ensembles)/Programme 9/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour créer une différence symétrique.
+"""
+
+# Création de deux ensembles avec quelques éléments
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {4, 5, 6, 7, 8}
+
+# Création de la différence symétrique entre les deux ensembles
+difference_symetrique = ensemble1 ^ ensemble2
+
+# Affichage du résultat
+print("La différence symétrique entre les deux ensembles est :", difference_symetrique)
