558c41f3d13545d9921d19f9add91440a60fc4ad
GalsenCode <galsencode@gmail.com>
2025-06-10 22:37:49 +0000

    Union Ensemble

---
+++ Set (Ensembles)/Programme 7/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour créer une union d’ensembles.
+"""
+
+# Création de deux ensembles avec quelques éléments
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {4, 5, 6, 7, 8}
+
+# Création de l'union des deux ensembles
+union = ensemble1 | ensemble2
+
+# Affichage du résultat
+print("L'union des deux ensembles est :", union)
