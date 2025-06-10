991924ef679fca21eaadbbce8dc34edf1b766aa7
GalsenCode <galsencode@gmail.com>
2025-06-10 22:43:18 +0000

    Difference Ensemble

---
+++ Set (Ensembles)/Programme 8/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour créer une différence entre les ensembles.
+"""
+
+# Création de deux ensembles avec quelques éléments
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {4, 5, 6, 7, 8}
+
+# Création de la différence entre les deux ensembles
+difference = ensemble1 - ensemble2
+
+# Affichage du résultat
+print("La différence entre les deux ensembles est :", difference)
