8ff8e8954722b30fa9ad960d8c317d874e3f4de6
GalsenCode <galsencode@gmail.com>
2025-06-10 22:52:55 +0000

    Sous Ensemble

---
+++ Set (Ensembles)/Programme 11/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour vérifier si un ensemble est un sous-ensemble d’un autre ensemble.
+"""
+
+# Création de deux ensembles avec quelques éléments
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {1, 2, 3}
+
+# Vérification si ensemble2 est un sous-ensemble de ensemble1
+est_sous_ensemble = ensemble2.issubset(ensemble1)
+
+# Affichage du résultat
+print("L'ensemble2 est un sous-ensemble de l'ensemble1 :", est_sous_ensemble)
