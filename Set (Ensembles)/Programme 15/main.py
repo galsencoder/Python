14091840b920eb5d4142b83628abc06e790268de
GalsenCode <galsencode@gmail.com>
2025-06-10 23:04:29 +0000

    Presence d'une valeur dans un ensemble

---
+++ Set (Ensembles)/Programme 15/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour vérifier si une valeur donnée est présente ou non dans un ensemble.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3, 4, 5}
+
+# Vérification de la présence d'une valeur dans l'ensemble
+valeur_a_verifier = 3
+est_present = valeur_a_verifier in ensemble
+
+# Affichage du résultat
+print("La valeur", valeur_a_verifier, "est présente dans l'ensemble :", est_present)
