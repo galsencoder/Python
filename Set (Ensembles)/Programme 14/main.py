af4da4274f56f4cb3915756ebaf542bdd3bf04e3
GalsenCode <galsencode@gmail.com>
2025-06-10 22:58:24 +0000

    Longueur d'un ensmble

---
+++ Set (Ensembles)/Programme 14/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour trouver la longueur d’un ensemble.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3, 4, 5}
+
+# Trouver la longueur de l'ensemble
+longueur = len(ensemble)
+
+# Affichage du résultat
+print("La longueur de l'ensemble est :", longueur)
+
