2c1a6de1d108a556fddaa0eb3d40cae823cc38ac
GalsenCode <galsencode@gmail.com>
2025-06-10 22:56:12 +0000

    Maximun - Ensemble

---
+++ Set (Ensembles)/Programme 13/main.py
@@ -0,0 +1,14 @@
+﻿"""
+Ecrivez un programme Python pour trouver les valeurs maximales et minimales d’un ensemble.
+"""
+
+# Création d'un ensemble avec quelques éléments
+ensemble = {1, 2, 3, 4, 5}
+
+# Trouver les valeurs maximales et minimales de l'ensemble
+valeur_maximale = max(ensemble)
+valeur_minimale = min(ensemble)
+
+# Affichage du résultat
+print("La valeur maximale de l'ensemble est :", valeur_maximale)
+print("La valeur minimale de l'ensemble est :", valeur_minimale)
