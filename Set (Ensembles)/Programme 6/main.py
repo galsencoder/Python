b19958f38212fc6df56e5f57233e00719560bd2a
GalsenCode <galsencode@gmail.com>
2025-06-10 22:35:32 +0000

    Intersection Ensemble

---
+++ Set (Ensembles)/Programme 6/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour créer une intersection d’ensembles.
+"""
+
+# Création de deux ensembles avec quelques éléments
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {4, 5, 6, 7, 8}
+
+# Création de l'intersection des deux ensembles
+intersection = ensemble1 & ensemble2
+
+# Affichage du résultat
+print("L'intersection des deux ensembles est :", intersection)
