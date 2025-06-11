69d626424068024b47bf43db3a8a037ffd6947cf
GalsenCode <galsencode@gmail.com>
2025-06-11 23:17:45 +0000

    Element commun dans un ensemble

---
+++ Set (Ensembles)/Programme 16/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écrire un programme Python pour vérifier si deux ensembles donnés n’ont pas d’éléments en commun.
+"""
+
+# Definition des ensembles
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {6, 7, 8, 9, 10}
+
+# Vérification des éléments communs
+if ensemble1.isdisjoint(ensemble2):
+    print("Les deux ensembles n'ont pas d'éléments en commun.")
+else:
+    print("Les deux ensembles ont des éléments en commun.")
