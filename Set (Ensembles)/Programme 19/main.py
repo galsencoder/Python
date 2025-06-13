c05c6ab38ef361b6b5aebc83c505d4c8340a7d5e
GalsenCode <galsencode@gmail.com>
2025-06-13 23:39:15 +0000

    Retourne un ensemble nettoyé

---
+++ Set (Ensembles)/Programme 19/main.py
@@ -0,0 +1,12 @@
+﻿"""
+Crée une fonction qui retourne un nouvel ensemble nettoyé, sans "" ni None en français.
+"""
+
+def nettoyer_ensemble(ensemble):
+    return {element for element in ensemble if element not in ("", None)}
+
+
+
+ensemble_exemple = {"chat", "", "chien", None, "oiseau", "poisson"}
+ensemble_nettoye = nettoyer_ensemble(ensemble_exemple)
+print(ensemble_nettoye)
