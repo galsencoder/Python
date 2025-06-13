6ff3a88c1da6340a0b982c008e05b43ec0a9c90d
GalsenCode <galsencode@gmail.com>
2025-06-13 23:41:12 +0000

    Retourne une liste sans les mots interdits

---
+++ Set (Ensembles)/Programme 20/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Écris une fonction filtrer(liste, interdits) qui retourne une nouvelle liste sans les mots interdits.
+"""
+
+def filtrer(liste, interdits):
+    return [mot for mot in liste if mot not in interdits]
+
+
+
+liste_exemple = ["chat", "chien", "oiseau", "poisson", "chat"]
+interdits_exemple = ["chat", "poisson"]
+liste_filtrée = filtrer(liste_exemple, interdits_exemple)
+print(liste_filtrée) 
