83f0f7b72a4b9d5939b4c1503437171178a1ded3
GalsenCode <galsencode@gmail.com>
2025-06-14 22:41:26 +0000

    Détecteur de mots uniques dans plusieurs textes

---
+++ Set (Ensembles)/Projet 1/main.py
@@ -0,0 +1,21 @@
+﻿"""
+Projet 1 : Détecteur de mots uniques dans plusieurs textes
+
+Objectif :
+Analyser plusieurs fichiers texte et déterminer :
+Les mots communs à tous les textes
+Les mots uniques à chaque texte
+Les mots présents dans au moins deux textes
+en français.
+"""
+
+def mots_du_fichier(nom_fichier):
+    try:
+        with open(nom_fichier, 'r', encoding='utf-8') as f:
+            texte = f.read()
+            # Nettoyage des caractères spéciaux et séparation des mots
+            mots = set(texte.lower().split())
+            return mots
+    except FileNotFoundError:
+        print(f"Le fichier {nom_fichier} n'a pas été trouvé.")
+        return set()
